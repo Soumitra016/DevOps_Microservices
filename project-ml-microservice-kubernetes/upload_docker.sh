02730685a829717e38863821d5397496b87ae394
soumitra <soumitrasingh016@gmail.com>
2022-11-08 17:42:34 +0530

    Changes

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,6 +6,7 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=soumitra016/udacity-ml-app
 
 # Step 2:  
 # Authenticate & tag
@@ -13,3 +14,6 @@ echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker login -u soumitra016
+docker tag udacity-ml-app $dockerpath:latest
+docker push $dockerpath:latest
